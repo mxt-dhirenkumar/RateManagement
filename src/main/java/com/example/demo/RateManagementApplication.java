10d12d311f438c8d2c4202b71bb4af91f0a0114e
DHIRENKUMA-W-LT\d.waghmare <d.waghmare@maxxton.com>
2025-10-17 11:24:49 +0530

    Initial Spring Boot project setup with Maven and application.yml

---
+++ src/main/java/com/example/demo/RateManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateManagementApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(RateManagementApplication.class, args);
+        System.out.println("Rate Management Application is running...");
+	}
+
+}
